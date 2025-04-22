7fbbf768e1d940507c2ef6140cb4ce35225df503
Caroline Rádlová <caroll.radlova@gmail.com>
2025-04-22 20:01:55 +0200

    plysaci

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import { render } from '@czechitas/render';
 import '../global.css';
 import './index.css';
 
-const Footer = () => {
+/*const Footer = () => {
   return (
     <footer>
       <p>Czechitas, nová patička</p>
@@ -20,22 +20,51 @@ const Odkazy = () => {
       <li><a href="#">Odkaz 4</a></li>
     </ul>
   )
+}*/
+
+const name1 = 'Silvestr';
+const image1 = 'https://kodim.cz/cms/assets/czechitas/js2/lekce/sablony-komponenty/cv-komponenty/plysaci/elephant.jpg';
+const text1 =
+'Silvestr rád pozoruje dění za oknem a upřímně se usmívá na všechny kolemjdoucí.'
+const name2 = 'Ctirad';
+const image2 = 'https://kodim.cz/cms/assets/czechitas/js2/lekce/sablony-komponenty/cv-komponenty/plysaci/mouse.jpg';
+const text2 =
+'Ctirad tráví svůj čas v blízkosti lednice a s očekáváním pozoruje její bílé dveře.'
+
+const Plysak1 = () => {
+  return (
+    <div className="plushy">
+<img className="plushy__image" src={image1} />
+<h2 className="plushy__name">{name1}</h2>
+<p className="plushy__text">{text1}</p>
+</div>
+  )
+}
+
+const Plysak2 = () => {
+  return (
+    <div className="plushy">
+<img className="plushy__image" src={image2} />
+<h2 className="plushy__name">{name2}</h2>
+<p className="plushy__text">{text2}</p>
+</div>
+  )
 }
 
 
 document.querySelector('#root').innerHTML = render(
   <div className="container">
     <header>
-      <div className="logo"></div>
-      <h1>Webová aplikace - updated</h1>
+      <h1>Plyšáci</h1>
       <br />
-      <Odkazy />
+      <Plysak1 />
+      <br />
+      <Plysak2 />
     </header>
     <main>
-      <p>Nový řádek.</p>
+    <div className="plushies"></div>
     </main>
-    <Footer>
-      <p>Czechitas, Digitální akademie: Web</p>
-    </Footer>
+   
   </div>
 );
+
